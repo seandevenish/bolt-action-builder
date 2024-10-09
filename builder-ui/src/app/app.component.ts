cd40a9ba9e72873aec4e2296cf92a18e4a28b01f
Sean Devenish <sean.devenish@businessfitness.com.au>
2024-10-09 16:55:10 +1100

    UI goodness

--- builder-ui/src/app/app.component.ts
+++ builder-ui/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { UserNavComponent } from './user-nav/user-nav.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatIconModule, UserNavComponent],
+  imports: [RouterOutlet, MatIconModule, UserNavComponent, MatToolbarModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
