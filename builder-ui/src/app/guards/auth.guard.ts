0e537704da35d73a85547a2ff4fc51016611274f
Sean Devenish <sean.devenish@businessfitness.com.au>
2024-11-05 09:34:51 +1100

    Fix login routing/timing

--- builder-ui/src/app/guards/auth.guard.ts
+++ builder-ui/src/app/guards/auth.guard.ts
@@ -1,14 +1,20 @@
 import { CanActivateFn, Router } from '@angular/router';
 import { inject } from '@angular/core';
 import { AuthService } from '../../user/auth.service';
+import { map } from 'rxjs';
 
 export const authGuard: CanActivateFn = (route, state) => {
   const authService = inject(AuthService);
   const router = inject(Router);
 
-  if (!authService.state) {
-    router.navigate(['/login']); // Redirect to the login page if the user is not logged in
-    return false; // Block access to this page
-  }
-  return true; // Allow access to the requeseted page
+  return authService.state$.pipe(
+    map(user => {
+      if (user && user.uid) {
+        return true; // Allow access if the user is logged in
+      } else {
+        router.navigate(['/login']); // Redirect to the login page if not logged in
+        return false; // Block access
+      }
+    })
+  );
 };
