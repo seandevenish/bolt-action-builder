e7eee2eb9e97c9c944a72b69c6a88c38d08fd660
Sean Devenish <sean.devenish@businessfitness.com.au>
2024-11-04 16:53:59 +1100

    More minor improvements

--- builder-ui/src/app/app.component.ts
+++ builder-ui/src/app/app.component.ts
@@ -4,11 +4,12 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { UserNavComponent } from './user-nav/user-nav.component';
 import { CommonModule } from '@angular/common';
+import { MatMenuModule } from '@angular/material/menu';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterModule, RouterOutlet, MatIconModule, UserNavComponent, MatToolbarModule],
+  imports: [CommonModule, RouterModule, RouterOutlet, MatIconModule, UserNavComponent, MatToolbarModule, MatMenuModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
